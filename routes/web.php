1169238fb9c88cf2e2c24291290aca340fce08a2
kertiw <putukertiw@gmail.com>
2020-05-25 01:52:40 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/dashboard');
 });
 Route::resource('/dashboard', 'CovidController');
 Route::get('/positif','CovidController@index');
